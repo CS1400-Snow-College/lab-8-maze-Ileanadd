74f5ef7bb0314a354947ec71e6cee0fd71dbb1e9
Ileana Gonzalez <ileanagonzalez@Ileanas-MacBook-Pro.local>
2026-07-03 01:59:01 -0600

    Greeting: Done

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// Lab 8: Maze
+// Ileana Gonzalez - 07/01
+
+// This program reads a maze from a text file, displays it on the console, and lets the user move through the maze with the use of arrow keys while avoiding walls and trying to get to the exit.
